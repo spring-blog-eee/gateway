be716a1cb84eab30e45ea0156224c6184f12af88
mahoushoujyo-eee <1653545106@qq.com>
2025-07-26 22:15:50 +0800

    initialize

---
+++ src/main/java/com/atguigu/bloggateway/BlogGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.bloggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogGatewayApplication.class, args);
+    }
+
+}
